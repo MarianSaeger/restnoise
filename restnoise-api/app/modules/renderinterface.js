73835082526a12566e0680da11b1547435ce4b7f
Marian Saeger <marian@flaregames.com>
2014-11-14 21:32:49 +0100

    Formatted code

--- restnoise-api/app/modules/renderinterface.js
+++ restnoise-api/app/modules/renderinterface.js
@@ -14,17 +14,17 @@ var Gradients = {
         [ 1, 255, 255, 255, 255 ]
     ],
 
-    grayscale : [
-      [-1, 0, 0, 0, 255 ],
-      [1,255,255,255,255]
+    grayscale: [
+        [-1, 0, 0, 0, 255 ],
+        [1, 255, 255, 255, 255]
     ],
 
-    jade : [
-        [ -1.000,  24, 146, 102, 255 ],
-        [  0.000,  78, 154, 115, 255 ],
+    jade: [
+        [ -1.000, 24, 146, 102, 255 ],
+        [  0.000, 78, 154, 115, 255 ],
         [  0.250, 128, 204, 165, 255 ],
-        [  0.375,  78, 154, 115, 255 ],
-        [  1.000,  29, 135, 102, 255 ]
+        [  0.375, 78, 154, 115, 255 ],
+        [  1.000, 29, 135, 102, 255 ]
     ],
 
     realistic: [
@@ -46,145 +46,147 @@ var Gradients = {
 function randomFileName() {
     function randomHash(count) {
         if (count === 1)
-            return parseInt(16*Math.random(), 10).toString(16);
+            return parseInt(16 * Math.random(), 10).toString(16);
         else {
             var hash = '';
-            for (var i=0; i<count; i++)
+            for (var i = 0; i < count; i++)
                 hash += randomHash(1);
             return hash;
         }
     }
 
-    return 'tmpfile_'+randomHash(4);
+    return 'tmpfile_' + randomHash(4);
 }
 
-function renderNetwork( network, rendermodulename, maptype, substitutevariables, gradient, gradientscale, callback ) {
+function renderNetwork(network, rendermodulename, maptype, substitutevariables, gradient, gradientscale, callback) {
 
-    console.log(gradient,gradientscale);
+    console.log(gradient, gradientscale);
 
-    for( var idx in gradient ) {
+    for (var idx in gradient) {
         gradient[idx][0] = gradient[idx][0] * gradientscale;
     }
 
-    var rndfilename = network["name"]+"_"+rendermodulename+".bmp";
-    substitutevariables["tmpfile"] = "\""+rndfilename+"\"";
+    var rndfilename = network["name"] + "_" + rendermodulename + ".bmp";
+    substitutevariables["tmpfile"] = "\"" + rndfilename + "\"";
 
-    var modules = attachRenderNodes( network["modules"], rendermodulename, gradient, maptype );
-    var substitutionresult = substituteVariables( modules, substitutevariables );
+    var modules = attachRenderNodes(network["modules"], rendermodulename, gradient, maptype);
+    var substitutionresult = substituteVariables(modules, substitutevariables);
 
     modules = substitutionresult.modules;
 
 
-    if ( Object.keys(substitutionresult.missingvars).length > 0 ) {
-        return callback({ error: "Missing vars! The contained vars have no default values and are not declared.", msg:Object.keys(substitutionresult.missingvars)});
+    if (Object.keys(substitutionresult.missingvars).length > 0) {
+        return callback({ error: "Missing vars! The contained vars have no default values and are not declared.", msg: Object.keys(substitutionresult.missingvars)});
     }
 
     // Determine hash of network, maybe we got it already
-    var nw_stringified = JSON.stringify( modules );
+    var nw_stringified = JSON.stringify(modules);
     var md5sum = crypto.createHash('md5');
     md5sum.update(nw_stringified);
     var chksum = md5sum.digest('hex');
-    console.log("md5",chksum);
+    console.log("md5", chksum);
 
     // Check if file exists already
-    var target_filename = network["name"]+"_"+chksum+".jpg";
+    var target_filename = network["name"] + "_" + chksum + ".jpg";
 
-    if ( fs.existsSync( "public/"+target_filename ) ) {
-        return callback("public/"+target_filename);
+    if (fs.existsSync("public/" + target_filename)) {
+        return callback("public/" + target_filename);
     }
 
-    fs.writeFileSync("./"+rndfilename+".conf", JSON.stringify( modules ) );
+    fs.writeFileSync("./" + rndfilename + ".conf", JSON.stringify(modules));
 
-    var child = exec('python ../rendernoise/rendernoise.py --network ./'+rndfilename+'.conf', function( error, stdout, stderr)
-    {
-        if ( error != null ) {
+    var child = exec('python ../rendernoise/rendernoise.py --network ./' + rndfilename + '.conf', function (error, stdout, stderr) {
+        if (error != null) {
 
             // error handling & exit
             console.log(stderr);
             console.log(stdout);
-            return callback({ error: "Error occured while rendering!", msg:error});
+            return callback({ error: "Error occured while rendering!", msg: error});
         }
         console.log(stdout);
 
 
-        im(rndfilename).write("public/"+target_filename, function () {
-            callback("public/"+target_filename);
-            setTimeout(function() {
+        im(rndfilename).write("public/" + target_filename, function () {
+            callback("public/" + target_filename);
+            setTimeout(function () {
                     //fs.unlink(rndfilename+".jpg", function(){} );
-                    fs.unlink(rndfilename, function(){} );
-                    fs.unlink(rndfilename+".conf", function(){} );
+                    fs.unlink(rndfilename, function () {
+                    });
+                    fs.unlink(rndfilename + ".conf", function () {
+                    });
                 }
-                , 5000 );
-        } );
+                , 5000);
+        });
     });
 }
 
 
-function attachRenderNodes( modules, rendermodulename, fancyGradientPoints, maptype ) {
+function attachRenderNodes(modules, rendermodulename, fancyGradientPoints, maptype) {
 
 
     // Attach render structure
     modules["renderer"] = {
-        "LightContrast" : "var:LightIntensity,0.000002894",
-        "LightIntensity" : "var:LightIntensity,2.0",
-        "LightElev":"var:LightElev,45.0",
-        "LightAzimuth":"var:LightAzimuth,135.0",
-        "EnableLight" : "var:EnableLight,true",
-        "GradientPoints" : fancyGradientPoints,
-        "DestFilename" : "var:tmpfile",
-        "SourceNoiseMap" : "heightMap",
-        "type" : "RendererImageNetwork"
+        "LightContrast": "var:LightContrast,0.000002894",
+        "LightIntensity": "var:LightIntensity,2.0",
+        "LightElev": "var:LightElev,45.0",
+        "LightAzimuth": "var:LightAzimuth,135.0",
+        "EnableLight": "var:EnableLight,true",
+        "GradientPoints": fancyGradientPoints,
+        "DestFilename": "var:tmpfile",
+        "SourceNoiseMap": "heightMap",
+        "type": "RendererImageNetwork"
     };
     modules["heightMapBuilder"] = {
-        "Bounds" : [ "var:bound0,1.0", "var:bound1,2.0", "var:bound2,3.0", "var:bound3,4.0" ],
-        "DestSize" : [ "var:width,512", "var:height,256" ],
-        "DestNoiseMap" : "heightMap",
-        "SourceModule" : rendermodulename,
-        "type" : maptype
+        "Bounds": [ "var:boundx0,-1.0", "var:boundx1,1.0", "var:boundy0,-1.0", "var:boundy1,1.0" ],
+        "DestSize": [ "var:width,512", "var:height,256" ],
+        "DestNoiseMap": "heightMap",
+        "SourceModule": rendermodulename,
+        "type": maptype
     };
     modules["heightMap"] = {
-        "type" : "NoiseMap"
+        "type": "NoiseMap"
     };
 
     return modules;
 }
 
 
-function substituteVariables( modules, substitutevariables ) {
-    var nw_stringified = JSON.stringify( modules );
+function substituteVariables(modules, substitutevariables) {
+    var nw_stringified = JSON.stringify(modules);
     var missingvars = {};
 
 
-    nw_stringified = nw_stringified.replace(/\"var:([^,\"]*),([^\"]*)\"/g, function(completematch,varname,defaultvalue) {
-        if ( substitutevariables.hasOwnProperty( varname ) ) {
+    nw_stringified = nw_stringified.replace(/\"var:([^,\"]*),([^\"]*)\"/g, function (completematch, varname, defaultvalue) {
+        if (substitutevariables.hasOwnProperty(varname)) {
             return substitutevariables[varname]
         } else {
             return defaultvalue
         }
 
-    } );
-    nw_stringified = nw_stringified.replace(/\"var:([^\"]*)\"/g, function(completematch,varname) {
-        if ( substitutevariables.hasOwnProperty( varname ) ) {
+    });
+    nw_stringified = nw_stringified.replace(/\"var:([^\"]*)\"/g, function (completematch, varname) {
+        if (substitutevariables.hasOwnProperty(varname)) {
             return substitutevariables[varname]
         } else {
             missingvars[varname] = 1;
-            return "MISSING VAR: "+varname;
+            return "MISSING VAR: " + varname;
         }
 
-    } );
+    });
 
 
-    modules = JSON.parse( nw_stringified );
-    return { modules : modules, missingvars : missingvars };;
+    modules = JSON.parse(nw_stringified);
+    return { modules: modules, missingvars: missingvars };
+    ;
 }
 
-function getGradient( gradientid ) {
-    return JSON.parse( JSON.stringify( Gradients[gradientid] ) );
+function getGradient(gradientid) {
+    return JSON.parse(JSON.stringify(Gradients[gradientid]));
 }
 
 module.exports = {
-    attachRenderNodes : attachRenderNodes,
-    renderNetwork : renderNetwork,
-    substituteVariables : substituteVariables,
-    getGradient : getGradient
+    attachRenderNodes: attachRenderNodes,
+    renderNetwork: renderNetwork,
+    substituteVariables: substituteVariables,
+    getGradient: getGradient
 };
